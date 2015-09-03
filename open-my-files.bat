8d8ef84a1267c48613cd1a6f45339eaadf4e319f
aaronistheman <aarons.7007@gmail.com>
2015-09-02 21:08:42 -0700

    Added the files needed for unit testing

--- open-my-files.bat
+++ open-my-files.bat
@@ -2,4 +2,7 @@ start Notepad++ index.html
 start Notepad++ README.md
 start Notepad++ css/deal-fusion.css
 start Notepad++ js/deal-fusion.js
+start Notepad++ todo-list.txt
+start Notepad++ unit-tests.html
+start Notepad++ js/unit-tests.js
 exit
