6bc763aedcba67714c400014cb12b364c8978260
aaronistheman <aarons.7007@gmail.com>
2015-09-02 21:31:05 -0700

    Set up a blank webpage and settled on a terse name for the game

--- open-my-files.bat
+++ open-my-files.bat
@@ -1,7 +1,7 @@
 start Notepad++ index.html
 start Notepad++ README.md
-start Notepad++ css/deal-fusion.css
-start Notepad++ js/deal-fusion.js
+start Notepad++ css/game-show-fusion.css
+start Notepad++ js/game-show-fusion.js
 start Notepad++ todo-list.txt
 start Notepad++ unit-tests.html
 start Notepad++ js/unit-tests.js
