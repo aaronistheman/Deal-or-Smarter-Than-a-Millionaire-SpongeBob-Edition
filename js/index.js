66e9ee5964cb85c86c5662a40d6f57220c9f6b9b
aaronistheman <aarons.7007@gmail.com>
2015-09-05 22:07:29 -0700

    User can hit Enter to go to game from menu

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
+var KEYCODES = {};
+KEYCODES.ENTER = 13;
+
 function drawMenuBackground() {
-    var canvas = document.getElementById("menu-background-canvas");
+    // var canvas = document.getElementById("menu-background-canvas");
+    // var ctx = canvas.getContext('2d');
+    // ctx.beginPath();
+    // ctx.moveTo(100, 50);
+    // ctx.lineTo(300, 50);
+    // ctx.stroke();
+}
+
+function drawMenuText() {
+    var canvas = document.getElementById("menu-canvas");
     var ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.moveTo(100, 50);
-    ctx.lineTo(300, 50);
-    ctx.stroke();
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText("Press Enter to play", canvas.width / 2,
+        canvas.height / 2);
 }
 
 $(document).ready(function() {
-    drawMenuBackground();
+    // drawMenuBackground();
+
+    drawMenuText();
+
+    $(document).keydown(function(e) {
+        if (e.which === KEYCODES.ENTER)
+            window.location.href = "play.php";
+    });
 });
