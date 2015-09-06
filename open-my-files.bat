8bbc3ebb5dec7f41c20d1fd596d2c410dd7b1c1e
aaronistheman <aarons.7007@gmail.com>
2015-09-06 11:31:29 -0700

    Combined main menu and game into one webpage

--- open-my-files.bat
+++ open-my-files.bat
@@ -1,10 +1,8 @@
 start Notepad++ index.html
 start Notepad++ README.md
 start Notepad++ css/game-show-fusion.css
-start Notepad++ js/index.js
+start Notepad++ js/game-show-fusion.js
 start Notepad++ todo-list.txt
 start Notepad++ unit-tests.html
 start Notepad++ js/unit-tests.js
-start Notepad++ play.html
-start Notepad++ js/play.js
 exit
