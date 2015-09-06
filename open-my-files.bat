dfd48edd6485abd91e4f60aabc1a87ddc81cb304
aaronistheman <aarons.7007@gmail.com>
2015-09-06 11:36:38 -0700

    Moved the AngularJS controller code into its own file

--- open-my-files.bat
+++ open-my-files.bat
@@ -5,4 +5,6 @@ start Notepad++ js/game-show-fusion.js
 start Notepad++ todo-list.txt
 start Notepad++ unit-tests.html
 start Notepad++ js/unit-tests.js
+start Notepad++ js/game-controller.js
+start Notepad++ js/game-controller.js
 exit
