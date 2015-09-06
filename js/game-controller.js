b67cd4d1c5907e95123efcb5de672090608f8213
aaronistheman <aarons.7007@gmail.com>
2015-09-06 15:06:30 -0700

    Linked the main menu to the game again

--- js/game-controller.js
+++ js/game-controller.js
@@ -3,12 +3,13 @@ app.controller('gameCtrl', function($scope) {
     // make an array for ids of each canvas in this webpage
     var canvasIds = [];
     canvasIds.push("menu-canvas");
-    canvasIds.push("menu-background-canvas");
+    // canvasIds.push("menu-background-canvas");
     canvasIds.push("choose-question-canvas");
-    canvasIds.push("choose-question-background-canvas");
+    // canvasIds.push("choose-question-background-canvas");
     canvasIds.push("questioning-canvas");
-    canvasIds.push("questioning-background-canvas");
+    // canvasIds.push("questioning-background-canvas");
 
+    // insert:
     // open case scene
     // banker deal scene
 
