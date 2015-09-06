2c3d7c4ef38dfb77bf95cf88c6513c70d6ddbef5
aaronistheman <aarons.7007@gmail.com>
2015-09-05 20:30:47 -0700

    Separated the code into code involving the main menu and code involving the game
    Thus, this won't be a single page application.

--- open-my-files.bat
+++ open-my-files.bat
@@ -1,8 +1,10 @@
 start Notepad++ index.html
 start Notepad++ README.md
 start Notepad++ css/game-show-fusion.css
-start Notepad++ js/game-show-fusion.js
+start Notepad++ js/index.js
 start Notepad++ todo-list.txt
 start Notepad++ unit-tests.html
 start Notepad++ js/unit-tests.js
+start Notepad++ play.php
+start Notepad++ js/play.js
 exit
