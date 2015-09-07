84a279af7852fad13617f524673c74f5ecd9d4b3
aaronistheman <aarons.7007@gmail.com>
2015-09-07 12:51:29 -0700

    Gave up on function drawQuotes()
    I worked on it for about two or three hours, and I still couldn't figure
    it out. Therefore, I've saved my work and wrote some documentation about
    it in the file, but the function is unusable. It's quite unfortunate, but
    I think anymore time put into that function would make the benefits of
    implementing that function not worth the effort. I'll have to deal with
    drawQuoteText().

--- js/game-show-fusion.js
+++ js/game-show-fusion.js
@@ -71,23 +71,24 @@ function eraseQuotes() {
 }
 
 /*
-  @pre canvases are set up
-  @post text has been drawn with appropriate wrap around; endCallback
-  is set up to be called when user wants
-  @hasTest no
-  @param text to draw
-  @param lengthForWrapAround maximum number of characters for each
-  row of quote
-  @param endCallback to call after the user presses Enter (can
-  be used for chaining quote bubbles together) (optional)
-  @returns nothing
-  @throws nothing
+    @pre canvases are set up
+    @post text has been drawn with appropriate wrap around and in quote
+    bubble; endCallback is set up to be called when user wants
+    @hasTest no
+    @param text to draw
+    @param lengthForWrapAround maximum number of characters for each
+    row of quote
+    @param endCallback to call after the user presses Enter (can
+    be used for chaining quote bubbles together) (optional)
+    @returns nothing
+    @throws nothing
 */
 function drawQuoteText(text, lengthForWrapAround, endCallback) {
     drawEachTextPiece(convertStringToArrayOfStrings(text,
         lengthForWrapAround));
 
     if (endCallback !== undefined) {
+        // Allow the endCallback to be called
         $(document).keydown(function(e) {
             if (e.which === KEYCODES.ENTER) {
                 $(document).off("keydown");
@@ -101,20 +102,136 @@ function drawQuoteText(text, lengthForWrapAround, endCallback) {
             if (e.which === KEYCODES.ENTER) {
                 $(document).off("keydown");
                 gameShow.nextQuoteSound.play();
-                eraseQuotes();
             }
         });
     }
 }
 
 /*
-  @pre canvases are set up
-  @post each string in textPieces has been drawn, with each string
-  using its own row
-  @hasTest no
-  @param textPieces array of strings to draw on canvas
-  @returns nothing
-  @throws nothing
+    NOTE: The following is an unfinished function that would have
+    facilitated the chaining of quotes. I worked on it for about
+    two or three hours before giving up, for I can't identify
+    the problem. I don't think the benefits of the function are
+    worth risking any more time figuring it out. Thus, immediately
+    below is the function's contract, and with comments, I've
+    divided the function into the two different implementations
+    I tried.
+
+    It would've been used like this:
+    drawQuotes(["Quote 1", "Quote 2", "Quote 3", "Quote 4"], 85,
+        function() {
+            alert("Finished the chain");
+        });
+
+    @pre canvases are set up
+    @post each quote in arrayOfQuotes has been set up in chain to
+    appear in the quote bubble;
+    endCallback has been set up to be called at end of chain
+    @hasTest no
+    @param arrayOfQuotes to set up to be drawn
+    @param lengthForWrapAround maximum number of characters in each
+    row of quote bubble
+    @param finalCallback to call after the user has gone through the
+    chain of quotes
+    @returns nothing
+    @throws nothing
+*/
+// function drawQuotes(arrayOfQuotes, lengthForWrapAround, finalCallback) {
+    /*
+        THIS WAS THE SECOND (PROBABLY BETTER) IMPLEMENTATION I TRIED.
+    */
+    // In reverse order, make the function for drawing each
+    // quote so each can be chained together
+    /*var endingCallbacks = [];
+    for (var i = arrayOfQuotes.length - 1; i >= 0; --i) {
+        var action = undefined;
+        // var nextQuote = undefined;
+        // var chainCallback = undefined;
+        if ((i + 1) === arrayOfQuotes.length) {
+            action = function() {
+                gameShow.nextQuoteSound.play();
+                eraseQuotes();
+                finalCallback();
+            };
+
+            endingCallbacks.push({endCallback : action});
+
+        }
+        else {
+            var object = {};
+            object.nextQuote = arrayOfQuotes[i + 1];
+            object.chainCallback =
+                endingCallbacks[endingCallbacks.length - 1];
+            object.endCallback = function() {
+                alert(this);
+                drawQuoteText(this.nextQuote,
+                    lengthForWrapAround,
+                    this.chainCallbackObject.endCallback);
+                gameShow.nextQuoteSound.play();
+            };
+
+            endingCallbacks.push(object);
+
+            // endingCallbacks.push({endCallback : action,
+                // nextQuote : arrayOfQuotes[i + 1],
+                // chainCallbackObject :
+                    // endingCallbacks[endingCallbacks.length - 1]});
+        }
+    }
+
+    // Start the chain
+    drawQuoteText(arrayOfQuotes[0], lengthForWrapAround,
+        endingCallbacks[endingCallbacks.length - 1].endCallback);
+    */
+
+
+    /*
+        THIS WAS THE FIRST IMPLEMENTATION I TRIED.
+    */
+    // In reverse order, make the function for drawing each
+    // quote so each can be chained together
+    /* var endingCallbacks = [];
+    for (var i = arrayOfQuotes.length - 1; i >= 0; --i) {
+        // Check if this is the last quote
+        if ((i + 1) === arrayOfQuotes.length)
+            endingCallbacks.push(function() {
+                gameShow.nextQuoteSound.play();
+                eraseQuotes();
+                endCallback();
+            });
+        else {
+            // there is another quote to chain
+            // alert("Quote: " + arrayOfQuotes[i + 1]);
+            // alert("endCallbackIndex: " + (endingCallbacks.length - 1));
+            // endingCallbacks.push(function() {
+                // drawQuoteText(arrayOfQuotes[i + 1],
+                    // lengthForWrapAround,
+                    // endingCallbacks[endingCallbacks.length - 1]);
+                // gameShow.nextQuoteSound.play();
+            // });
+            // endingCallbacks.push(function() {
+                // drawQuoteText(arrayOfQuotes.slice(i + 1, 1),
+                    // lengthForWrapAround,
+                    // endingCallbacks.slice(
+                        // endingCallbacks.length - 1, 1));
+                // gameShow.nextQuoteSound.play();
+            // });
+        }
+
+    // Start the chain
+    // drawQuoteText(arrayOfQuotes[0], lengthForWrapAround,
+        // endingCallbacks[endingCallbacks.length - 1]);
+    */
+// }
+
+/*
+    @pre canvases are set up
+    @post each string in textPieces has been drawn, with each string
+    using its own row
+    @hasTest no
+    @param textPieces array of strings to draw on canvas
+    @returns nothing
+    @throws nothing
 */
 function drawEachTextPiece(textPieces) {
     var canvas = document.getElementById('quote-text-canvas');
@@ -136,14 +253,14 @@ function drawEachTextPiece(textPieces) {
 }
 
 /*
-  @pre none
-  @post see @returns
-  @hasTest yes
-  @param string to split into pieces
-  @param maxStringLength for each string in the array
-  @returns array of pieces of the parameter string such that none
-  of the pieces have a length greater than maxStringLength
-  @throws nothing
+    @pre none
+    @post see @returns
+    @hasTest yes
+    @param string to split into pieces
+    @param maxStringLength for each string in the array
+    @returns array of pieces of the parameter string such that none
+    of the pieces have a length greater than maxStringLength
+    @throws nothing
 */
 function convertStringToArrayOfStrings(string, maxStringLength) {
     var textPieces = [];
@@ -169,13 +286,10 @@ function setUpGame() {
     $(document).off("keydown");
     drawSpongebob();
     setUpQuoteBubble();
-    /*drawQuotes(["Quote 1", "Quote 2", "Quote 3", "Quote 4"], 85,
-        function() {
-            alert("Finished the chain");
-        });*/
     drawQuoteText("Quote 1", 85, function() {
        drawQuoteText("Quote 2", 85, function() {
             alert("Finished the chain");
+            eraseQuotes();
        });
     });
 }
