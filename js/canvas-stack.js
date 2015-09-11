a14549353b7043200904d1d46a76c79e6fe87564
aaronistheman <aarons.7007@gmail.com>
2015-09-10 21:48:22 -0700

    Moved the definitions of CanvasStack's non-static methods outside of it

--- js/canvas-stack.js
+++ js/canvas-stack.js
@@ -7,68 +7,68 @@
 
 function CanvasStack() {
     this.storage = [];
-
-    /*
-        @pre none
-        @post whatToAdd has been added to this.storage, and the
-        canvas represented by whatToAdd has been given CSS class
-        'show'
-        @hasTest yes
-        @param whatToAdd to affect as the postcondition specifies;
-        must be a value in CanvasStack.CANVASES_IDS
-        @returns "this" pointer
-        @throws nothing
-    */
-    this.add = function(whatToAdd) {
-        if (typeof whatToAdd == "string") {
-            // only one canvas to add and show
-            this.storage.push(whatToAdd);
-            $('#' + whatToAdd).addClass('show');
-        }
-        else {
-            // array of canvases to add
-            this.storage = this.storage.concat(whatToAdd);
-            for (var i in whatToAdd)
-                $('#' + whatToAdd[i]).addClass('show');
-        }
-        return this;
-    };
-
-    /*
-        @pre none
-        @post the canvas or canvases commanded to be removed have
-        been removed and (via CSS) hidden
-        @hasTest yes
-        @param whatToRemove which canvas or canvases to remove;
-        pass an array of canvases to remove all of those;
-        must be one of the constants attached to
-        CanvasStack.CANVASES_IDS
-        @returns "this" pointer
-        @throws nothing
-    */
-    this.remove = function(whatToRemove) {
-        // React according to how many canvas ids to remove
-        var canvasIdsToRemove = [];
-        if (typeof whatToRemove == "string")
-            canvasIdsToRemove.push(whatToRemove);
-        else
-            canvasIdsToRemove = whatToRemove;
-
-        for (var i in canvasIdsToRemove) {
-            for (var j = 0; j < this.storage.length; ) {
-                if (this.storage[j] === canvasIdsToRemove[i]) {
-                    $('#' + this.storage[j]).removeClass('show');
-                    this.storage.splice(j, 1);
-                    break;
-                }
-                else
-                    ++j;
-            }
-        }
-
-        return this;
-    };
 }
+
+/*
+    @pre none
+    @post whatToAdd has been added to this.storage, and the
+    canvas represented by whatToAdd has been given CSS class
+    'show'
+    @hasTest yes
+    @param whatToAdd to affect as the postcondition specifies;
+    must be a value in CanvasStack.CANVASES_IDS
+    @returns "this" pointer
+    @throws nothing
+*/
+CanvasStack.prototype.add = function(whatToAdd) {
+    if (typeof whatToAdd == "string") {
+        // only one canvas to add and show
+        this.storage.push(whatToAdd);
+        $('#' + whatToAdd).addClass('show');
+    }
+    else {
+        // array of canvases to add
+        this.storage = this.storage.concat(whatToAdd);
+        for (var i in whatToAdd)
+            $('#' + whatToAdd[i]).addClass('show');
+    }
+    return this;
+};
+
+/*
+    @pre none
+    @post the canvas or canvases commanded to be removed have
+    been removed and (via CSS) hidden
+    @hasTest yes
+    @param whatToRemove which canvas or canvases to remove;
+    pass an array of canvases to remove all of those;
+    must be one of the constants attached to
+    CanvasStack.CANVASES_IDS
+    @returns "this" pointer
+    @throws nothing
+*/
+CanvasStack.prototype.remove = function(whatToRemove) {
+    // React according to how many canvas ids to remove
+    var canvasIdsToRemove = [];
+    if (typeof whatToRemove == "string")
+        canvasIdsToRemove.push(whatToRemove);
+    else
+        canvasIdsToRemove = whatToRemove;
+
+    for (var i in canvasIdsToRemove) {
+        for (var j = 0; j < this.storage.length; ) {
+            if (this.storage[j] === canvasIdsToRemove[i]) {
+                $('#' + this.storage[j]).removeClass('show');
+                this.storage.splice(j, 1);
+                break;
+            }
+            else
+                ++j;
+        }
+    }
+
+    return this;
+};
 
 /*
     'Static' members and instances
